f81a2dc0545ce389c9f3c9e5259a0a8db0802e61
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-23 16:57:47 +0530

    Delete src/main/java/com/example/securesearch/SearchRepository.java

--- src/main/java/com/example/securesearch/SearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.securesearch.repository;
-
-import com.example.securesearch.model.SearchItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SearchRepository extends MongoRepository<SearchItem, String> {
-}
