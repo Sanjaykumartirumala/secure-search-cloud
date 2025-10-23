cc048086710c454071f8559a773a8d7ad19804e5
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-23 16:32:38 +0530

    Create FileDocument.java

---
+++ src/main/java/com/example/securesearch/model/FileDocument.java
@@ -0,0 +1,25 @@
+package com.example.securesearch.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "files")
+public class FileDocument {
+    @Id
+    private String id;
+    private String filename;
+    private String content;
+
+    public FileDocument() {}
+    public FileDocument(String filename, String content) {
+        this.filename = filename;
+        this.content = content;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFilename() { return filename; }
+    public void setFilename(String filename) { this.filename = filename; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
