21bc76616c89539159d11e7c573a4837a4634b54
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-23 16:58:00 +0530

    Delete src/main/java/com/example/securesearch/FileRepository.java

--- src/main/java/com/example/securesearch/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.securesearch.repository;
-
-import com.example.securesearch.model.FileDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository extends MongoRepository<FileDocument, String> {
-}
