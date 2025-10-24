6b5e69c0207feda7ec6738cb1fc959c8ba211e6c
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-24 12:19:28 +0530

    Change FileRepository to use EncryptedFile model

--- src/main/java/com/example/securesearch/repository/FileRepository.java
+++ src/main/java/com/example/securesearch/repository/FileRepository.java
@@ -1,9 +1,8 @@
 package com.example.securesearch.repository;
 
-import com.example.securesearch.model.FileDocument;
+import com.example.securesearch.model.EncryptedFile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FileRepository extends MongoRepository<FileDocument, String> {
-}
+public interface FileRepository extends MongoRepository<EncryptedFile, String> {}
