596c154888d11228f2be99f6d64e702a084c4325
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-24 11:06:53 +0530

    Update SearchRepository.java

--- src/main/java/com/example/securesearch/repository/SearchRepository.java
+++ src/main/java/com/example/securesearch/repository/SearchRepository.java
@@ -2,6 +2,8 @@ package com.example.securesearch.repository;
 
 import com.example.securesearch.model.SearchItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SearchRepository extends MongoRepository<SearchItem, String> {
 }
