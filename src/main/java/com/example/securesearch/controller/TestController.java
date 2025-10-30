a0fd46c9d49947e1e8656f22da24e220e49cc0ee
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2025-10-30 18:07:19 +0530

    Update TestController.java

--- src/main/java/com/example/securesearch/controller/TestController.java
+++ src/main/java/com/example/securesearch/controller/TestController.java
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-package com.example.demo.controller;
-
-import com.example.demo.model.TestDocument;
-import com.example.demo.repository.TestDocumentRepository;
-=======
 package com.example.securesearch.controller;
 
 import com.example.securesearch.model.TestDocument;
 import com.example.securesearch.repository.TestDocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> ff75c05 (Added mvnw.cmd and updated Dockerfile for Render deployment)
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,21 +11,6 @@ import java.util.List;
 @RequestMapping("/test")
 public class TestController {
 
-<<<<<<< HEAD
-    private final TestDocumentRepository repository;
-
-    public TestController(TestDocumentRepository repository) {
-        this.repository = repository;
-    }
-
-    // Create a document
-    @PostMapping("/create")
-    public TestDocument createDocument(@RequestParam String message) {
-        return repository.save(new TestDocument(message));
-    }
-
-    // Get all documents
-=======
     @Autowired
     private TestDocumentRepository repository;
 
@@ -43,7 +21,6 @@ public class TestController {
     }
 
     // Get all TestDocuments
->>>>>>> ff75c05 (Added mvnw.cmd and updated Dockerfile for Render deployment)
     @GetMapping("/all")
     public List<TestDocument> getAllDocuments() {
         return repository.findAll();
